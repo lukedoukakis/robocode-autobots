addf2026609b53ae8ae27ba4813a9d09cddec1c4
Nicholas Baron <njbaron@cpp.edu>
2018-01-25 23:03:05 -0800

    Tracking Target
    Finds and kills

--- Autobot/src/autobots/Auto.java
+++ Autobot/src/autobots/Auto.java
@@ -1,22 +1,98 @@
 package autobots;
 
+import java.awt.*;
+
+import robocode.*;
 import robocode.Robot;
 
 public class Auto extends Robot {
-	@Override
-	public void run() {
-		init();
-		while (true) {
-			update();
-		}
-	}
 
-	private void update() {
-		fire(10);
-		turnLeft(1);
-	}
+    private double targetDir;
+    private boolean trackingFlag = false;
 
-	private void init() {
-		System.out.println(this.getTime());
+    private boolean turnGunLeftFlag = false, turnRadarLeftFlag = false;
+
+    @Override
+    public void run() {
+	init();
+	while (true) {
+	    update();
 	}
+    }
+
+    private void update() {
+
+	if (trackingFlag) {
+	    if (closeEnough()) {
+		fire(3);
+	    }
+	    if (targetDir < getGunHeading()) {
+		turnGunLeft(1);
+	    } else if (targetDir > getGunHeading()) {
+		turnGunRight(1);
+	    }
+	} else {
+	    ahead(1);
+
+	    if (this.getGunHeading() >= 359) {
+		turnGunLeftFlag = true;
+	    }
+	    if (this.getGunHeading() <= 0) {
+		turnGunLeftFlag = false;
+	    }
+	    if (!turnGunLeftFlag) {
+		turnGunRight(5);
+	    } else {
+		turnGunLeft(5);
+	    }
+
+	    if (this.getRadarHeading() >= 359) {
+		turnRadarLeftFlag = true;
+	    }
+	    if (this.getRadarHeading() <= 0) {
+		turnRadarLeftFlag = false;
+	    }
+	    if (!turnRadarLeftFlag) {
+		turnRadarRight(5);
+	    } else {
+		turnRadarLeft(5);
+	    }
+	}
+    }
+
+    private void init() {
+	setAdjustGunForRobotTurn(true);
+	setAdjustRadarForGunTurn(true);
+	setAdjustRadarForRobotTurn(true);
+    }
+
+    private boolean closeEnough() {
+	return Math.abs(targetDir - this.getGunHeading()) <= 1;
+    }
+
+    @Override
+    public void onScannedRobot(ScannedRobotEvent event) {
+	targetDir = getHeading() + event.getBearing();
+	targetDir = fixToHeading(targetDir);
+	trackingFlag = true;
+    }
+
+    public static double fixToHeading(double val) {
+	double toRet = val;
+	while (toRet >= 360) {
+	    toRet -= 360;
+	}
+	while (toRet < 0) {
+	    toRet += 360;
+	}
+	return toRet;
+    }
+
+    @SuppressWarnings("boxing")
+    @Override
+    public void onPaint(Graphics2D g) {
+	g.setColor(Color.red);
+	g.drawString(String.format("%.2f", targetDir), (int) this.getX(), (int) this.getY() + 10);
+	g.drawString(String.format("%.2f", getGunHeading()), (int) this.getX(), (int) this.getY() + 20);
+    }
 }
