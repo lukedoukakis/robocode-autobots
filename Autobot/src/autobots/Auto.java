92597f87ee59185a1552932a765db0f459b59c49
Nicholas Baron <njbaron@cpp.edu>
2018-02-25 17:58:04 -0800

    Wobbling the radar and searching multiple targets
    UNTESTED!

--- Autobot/src/autobots/Auto.java
+++ Autobot/src/autobots/Auto.java
@@ -1,11 +1,15 @@
 package autobots;
 
+import java.util.*;
+
 import robocode.*;
 import robocode.util.Utils;
 
 public class Auto extends Robot {
 
     private static final double radarSpeed = 7.5;
+
+    private static final double wobbleIncrement = 5;
 
     private static int shortestTurnRoute(final double current, final double target) {
 
@@ -17,12 +21,13 @@ public class Auto extends Robot {
 	    return 0;
 	}
     }
-//Tracks 5-7 targets
-    private ArrayList<TargetDetails> target;
 
-    private boolean trackingFlag = false;
+    // Tracks 5-7 targets
+    private ArrayList<TargetDetails> target = new ArrayList<>();
 
     private boolean turnRadarLeftFlag = false;
+    private double lastWobbleTarget, wobbleExtreme = wobbleIncrement;
+    private byte lastWobbleDir = 1;
 
     /**
      * Returns whether if the current gun heading is within 1 degree of the target
@@ -31,51 +36,87 @@ public class Auto extends Robot {
 	return Math.abs(Utils.normalAbsoluteAngleDegrees(currentHeading) - getGunHeading()) <= deviation;
     }
 
+    /**
+     *
+     * @param useAngle
+     *            Whether to return the closest target by angle
+     * @return The closest target to the robot's current postion
+     */
+    private TargetDetails getClosestTarget(final boolean useAngle) {
+	int indexClosestDist = -1, indexClosestAngle = -1;
+	double closestDistSquared = Double.MAX_VALUE;
+	double closestAngle = Double.MAX_VALUE;
+
+	for (int i = 0; i < target.size(); i++) {
+	    final TargetDetails current = target.get(i);
+	    final double distSquared = current.getSqrDistance(getX(), getY());
+	    double angle = current.getAbsoluteHeadingDegreesFrom(getX(), getY()) - getGunHeading();
+	    angle = Math.abs(angle);
+
+	    if (distSquared < closestDistSquared) {
+		indexClosestDist = i;
+		closestDistSquared = distSquared;
+	    }
+
+	    if (angle < closestAngle) {
+		indexClosestAngle = i;
+		closestAngle = angle;
+	    }
+	}
+
+	if (useAngle) {
+	    return target.get(indexClosestAngle);
+	}
+
+	return target.get(indexClosestDist);
+    }
+
     private void init() {
 	setAdjustGunForRobotTurn(true);
 	setAdjustRadarForGunTurn(true);
 	setAdjustRadarForRobotTurn(true);
     }
-	
-    public void navigate(){ //movement, tends to avoid the edges of the battlefield
-	
-	double x = getBattleFieldWidth();
-	double y = getBattleFieldHeight();
-	double heading=getHeading();
-			
-	Random r = new Random();
 
-	if(getX()<x/2 && getY()<y/2){//if west and south
-		turnRight(Math.abs(45-(heading+ r.nextInt(10)-5)));
+    public void navigate() { // movement, tends to avoid the edges of the battlefield
+
+	final double x = getBattleFieldWidth();
+	final double y = getBattleFieldHeight();
+	final double heading = getHeading();
+
+	// Secondary variables.
+	final double midX = x / 2;
+	final double midY = y / 2;
+	final double randHeading = (heading + (Math.random() * 10)) - 5; // The randomized part of the heading
+
+	if ((getX() < midX) && (getY() < midY)) {// if west and south
+	    turnRight(Math.abs(45 - randHeading));
 	}
-	if(getX()<x/2 && getY()>y/2){//if west and north
-		turnRight(Math.abs(135-(heading+ r.nextInt(10)-5)));
+	if ((getX() < midX) && (getY() > midY)) {// if west and north
+	    turnRight(Math.abs(135 - randHeading));
 	}
-	if(getX()>x/2 && getY()>y/2){//if east and north
-		turnLeft(Math.abs(225-(heading+ r.nextInt(10)-5)));
+	if ((getX() > midX) && (getY() > midY)) {// if east and north
+	    turnLeft(Math.abs(225 - randHeading));
 	}
-	if(getX()>x/2 && getY()<y/2){//if east and south
-		turnLeft(Math.abs(315-(heading+ r.nextInt(10)-5)));
+	if ((getX() > midX) && (getY() < midY)) {// if east and south
+	    turnLeft(Math.abs(315 - randHeading));
 	}
 	ahead(200);
     }
-	
-	
-    public void navigateSerpentine(){ //probably useless, but this is some basic serpentine movement
-	for(int i=0;i<6;i++){
-		ahead(10);
-		turnRight(10);
+
+    public void navigateSerpentine() { // probably useless, but this is some basic serpentine movement
+	for (int i = 0; i < 6; i++) {
+	    ahead(10);
+	    turnRight(10);
 	}
-	for(int i=0;i<6;i++){
-		ahead(10);
-		turnLeft(10);
+	for (int i = 0; i < 6; i++) {
+	    ahead(10);
+	    turnLeft(10);
 	}
-}
+    }
 
     @Override
     public void onScannedRobot(final ScannedRobotEvent event) {
-	target = new TargetDetails(event, this);
-	trackingFlag = true;
+	target.add(new TargetDetails(event, this));
     }
 
     @Override
@@ -89,11 +130,14 @@ public class Auto extends Robot {
 
     private void update() {
 
-	if (trackingFlag) {
+	if (!target.isEmpty()) {
+
+	    TargetDetails closest = getClosestTarget(true);
+
 	    // Gets angle to target 0 to 360
-	    double targetDir = target.getAbsoluteHeadingDegreesFrom(getX(), getY());
+	    final double targetDir = closest.getAbsoluteHeadingDegreesFrom(getX(), getY());
 	    // Calc optimal speed
-	    double gunSpeed = Math.min(Math.abs(getGunHeading() - targetDir), Rules.GUN_TURN_RATE);
+	    final double gunSpeed = Math.min(Math.abs(getGunHeading() - targetDir), Rules.GUN_TURN_RATE);
 
 	    out.println("Target Direction: " + targetDir + "\nGun turn speed: " + gunSpeed + "\n" + target.toString());
 
@@ -102,11 +146,12 @@ public class Auto extends Robot {
 	    }
 
 	    turnGunRight(shortestTurnRoute(getGunHeading(), targetDir) * gunSpeed);
-
+	    wobbleRadar(targetDir);
 	}
-	
+	// Replace the following line with the method for movement
 	ahead(1);
-//Set radar to a wobbly path (recenter on every scan)
+
+	// Set radar to a wobbly path (recenter on every scan)
 	if (getRadarHeading() >= 359) {
 	    turnRadarLeftFlag = true;
 	}
@@ -117,6 +162,40 @@ public class Auto extends Robot {
 	    turnRadarRight(radarSpeed);
 	} else {
 	    turnRadarLeft(radarSpeed);
+	}
+
+    }
+
+    /**
+     *
+     * @param targetDir
+     */
+    private void wobbleRadar(final double targetDir) {
+	if (targetDir != lastWobbleTarget) {
+	    turnRadarRight(shortestTurnRoute(getRadarHeading(), targetDir) * radarSpeed);
+	    lastWobbleTarget = targetDir;
+	    return;
+	}
+
+	double posDev = targetDir + (wobbleExtreme * lastWobbleDir);
+	double currentRadar = getRadarHeading();
+
+	if ((lastWobbleDir == 1) && (posDev > currentRadar)) {
+	    turnRadarRight(posDev - currentRadar);
+	    return;
+	} else if ((lastWobbleDir == -1) && (posDev < currentRadar)) {
+	    turnRadarRight(posDev - currentRadar);
+	    return;
+	}
+
+	if (currentRadar == posDev) {
+	    wobbleExtreme += wobbleIncrement;
+
+	    if (lastWobbleDir < 0) {
+		lastWobbleDir = 1;
+	    } else if (lastWobbleDir > 0) {
+		lastWobbleDir = -1;
+	    }
 	}
 
     }
